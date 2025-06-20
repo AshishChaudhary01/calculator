00090518afa4a0891ef311d92112b8dc657eeeed
Ashish Chaudhary <201731753+AshishChaudhary01@users.noreply.github.com>
2025-06-20 06:36:47 +0545

    Add function to handle inputs
    Add an event listener to capture any button click, and delegate the
    buttons' value to the respective function for processing.

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,22 @@ let Calculator = {
   equation: "",
   progress: 0, // 0: entering num1, 1: operator selected, 2: result
 }
+
+function handleInput(triggeredBtn){
+  if(!buttonsContainer.matches('button')) return;   // Handle exceptional case where input isn't a button
+  const inputType = triggeredBtn.dataset.value;
+  const value = triggeredBtn.value;
+  if (inputType == "operand") {
+    inputOperand(value);
+  } else if (inputType == "operator") {
+    inputOperator(value);
+  } else if (inputType == "option") {
+    inputOption(value);
+  }
+}
+
+const buttonsContainer = document.querySelector("buttons-container");
+buttonsContainer.addEventListener('click', (e) => {
+  const triggeredBtn = e.target;
+  handleInput(triggeredBtn);
+});
