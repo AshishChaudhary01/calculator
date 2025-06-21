17fc7332c302a3cdc69cdc0e537748b070b2446b
Ashish Chaudhary <201731753+AshishChaudhary01@users.noreply.github.com>
2025-06-21 13:30:28 +0545

    Reposition the handleInput function
    Repostion the handleInput function for better code structure.

--- assets/script.js
+++ assets/script.js
@@ -7,19 +7,6 @@ let Calculator = {
   progress: 0, // 0: entering num1, 1: operator selected, 2: result
 }
 
-function handleInput(triggeredBtn) {
-  if (!buttonsContainer.matches('button')) return;   // Handle exceptional case where input isn't a button
-  const inputType = triggeredBtn.dataset.value;
-  const value = triggeredBtn.value;
-  if (inputType == "operand") {
-    inputOperand(value);
-  } else if (inputType == "operator") {
-    inputOperator(value);
-  } else if (inputType == "option") {
-    inputOption(value);
-  }
-}
-
 function parseOperands() {
   Calculator.num1 = parseFloat(Calculator.num1);
   Calculator.num2 = parseFloat(Calculator.num2);
@@ -50,6 +37,19 @@ function divide() {
 
 function remainder() {
   returnnum1 % num2;
+}
+
+function handleInput(triggeredBtn) {
+  if (!buttonsContainer.matches('button')) return;   // Handle exceptional case where input isn't a button
+  const inputType = triggeredBtn.dataset.value;
+  const value = triggeredBtn.value;
+  if (inputType == "operand") {
+    inputOperand(value);
+  } else if (inputType == "operator") {
+    inputOperator(value);
+  } else if (inputType == "option") {
+    inputOption(value);
+  }
 }
 
 const buttonsContainer = document.querySelector("buttons-container");
