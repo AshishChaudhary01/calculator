0a895a6be351591f27e20de27970de72c2b19668
Ashish Chaudhary <201731753+AshishChaudhary01@users.noreply.github.com>
2025-06-21 10:14:06 +0545

    Add the operator functions
    Add operator functions : add, subtract, multiply, divide, remainder.

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,8 @@ let Calculator = {
   progress: 0, // 0: entering num1, 1: operator selected, 2: result
 }
 
-function handleInput(triggeredBtn){
-  if(!buttonsContainer.matches('button')) return;   // Handle exceptional case where input isn't a button
+function handleInput(triggeredBtn) {
+  if (!buttonsContainer.matches('button')) return;   // Handle exceptional case where input isn't a button
   const inputType = triggeredBtn.dataset.value;
   const value = triggeredBtn.value;
   if (inputType == "operand") {
@@ -18,6 +18,26 @@ function handleInput(triggeredBtn){
   } else if (inputType == "option") {
     inputOption(value);
   }
+}
+
+function add() {
+  return num1 + num2;
+}
+
+function subtract() {
+  returnnum1 - num2;
+}
+
+function multiply() {
+  returnnum1 * num2;
+}
+
+function divide() {
+  returnnum1 / num2;
+}
+
+function remainder() {
+  returnnum1 % num2;
 }
 
 const buttonsContainer = document.querySelector("buttons-container");
